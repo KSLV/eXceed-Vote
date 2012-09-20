0098aacf4c99661939d1350b7155849175224e4a
Beammagic <beam_magic@hotmail.com>
2012-09-20 10:17:14 +0700

    add voterOperation
    only method, return ยังไม่ได้set นะใส่กัน error เฉยๆ

---
+++ src/VoterOperation.java
@@ -0,0 +1,33 @@
+
+public class VoterOperation {
+	
+	private String getName(id id)
+	{
+		return "";
+	}
+	
+	private String getSureName(id id)
+	{
+		return "";
+	}
+	
+	private int getTotalBallot(id id)
+	{
+		return 0;
+	}
+	
+	private int getUsedBallot(id id)
+	{
+		return 0;
+	}
+	
+	private void setTotalBallot(id id,int total)
+	{
+		
+	}
+	
+	private void setUsedBallot(id id, int used)
+	{
+		
+	}
+}
