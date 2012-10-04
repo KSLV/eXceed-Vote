15e4dd462a029bcc49aba63d0b206feaaeb0b154
Beammagic <beam_magic@hotmail.com>
2012-10-04 10:13:24 +0700

    ADD JUnit

---
+++ src/VerificationProcessTest.java
@@ -0,0 +1,64 @@
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+
+public class VerificationProcessTest {
+
+	private LoginGUI lg;
+	
+	@Test
+	public void loginTestFinish() {
+		try {
+			lg = new LoginGUI();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		int id=0;
+		try {
+			id = lg.verifyLogin("ARLeader", "ARLeader");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		assertEquals(1,id);
+	}
+	
+	@Test
+	public void loginFail() {
+		try {
+			lg = new LoginGUI();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		int id=0;
+		try {
+			id = lg.verifyLogin("login", "1234");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		assertEquals(0,id);
+	}
+	
+	@Test
+	public void userNameTrue()
+	{
+		try {
+			lg = new LoginGUI();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			lg.verifyLogin("ARLeader", "ARLeader");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		assertEquals("ARLeader", lg.getTmpUserName());
+		assertEquals("ARLeader", lg.getTmpPassword());
+	}
+	
+	
+
+}
