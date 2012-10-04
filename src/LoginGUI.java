9d230f50ef0af6dca3edf1f2586151bcb09d634c
Beammagic <beam_magic@hotmail.com>
2012-10-04 11:48:48 +0700

    deleate paramiter of LoginGUI

--- src/LoginGUI.java
+++ src/LoginGUI.java
@@ -10,7 +10,7 @@ public class LoginGUI extends VerificationProcess{
 	private static String userListDir = "src//UsernameList.txt";
 	private static String passListDir = "src//PasswordList.txt";	
 	
-	public LoginGUI(String username, String password) throws IOException {
+	public LoginGUI() throws IOException {
 		super(userListDir, passListDir);
 	}
 
