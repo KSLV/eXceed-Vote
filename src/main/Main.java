55d7de8449ecb0e7810cf0822ebe9c4c17169163
Lattasit Haritaworn <Bolshave_K@hotmail.com>
2012-12-25 12:15:55 +0700

    Remove unnecessary log4j folder

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,25 +1,12 @@
 package main;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Scanner;
-
 import login.LoginGUI;
 import login.VerificationProcess;
 
 import servicelocator.ServiceLocator;
 import admin.CommissionerView;
 import admin.CommissionerController;
-import com.avaje.ebean.Ebean;
-
-import database.NBallot;
-import database.QuestionDescription;
-import database.User;
 import exceed.dao.DaoFactory;
-import exceed.dao.ExceedUserDao;
-import exceed.dao.QuestionDao;
 
 
 
@@ -32,8 +19,6 @@ public class Main {
 		DaoFactory.getInstance();
 		new login.LoginGUIController(new VerificationProcess(),new LoginGUI());
 		new CommissionerController(new CommissionerView());
-		
-		
 	}
 	
 	
